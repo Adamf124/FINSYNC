c0d7d7a4d747628c932b3606951ce21e2968e002
Adam <adam.ferguson124@gmail.com>
2025-04-21 16:41:34 -0400

    alt tags

--- finsync-dashboard/app/page.tsx
+++ finsync-dashboard/app/page.tsx
@@ -34,13 +34,13 @@ export default function Page() {
             width={1200}
             height={920}
             className="hidden md:block"
-            alt="Screenshots of the dashboard project showing desktop version"
+            alt="Desktop mockup"
           />
           <Image
             src="/finhero-mobile.png"
             width={560}
             height={620}
-            className=""
+            className="block md:hidden"
             alt="Screenshots of the dashboard project showing mobile version"
           />
         </div>
