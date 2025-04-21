7cce92d96a8617abd6fc09d423f74350df5355da
Adam <adam.ferguson124@gmail.com>
2025-04-21 16:41:54 -0400

    dashboard components

---
+++ finsync-dashboard/app/dashboard/customers/page.tsx
@@ -0,0 +1,13 @@
+export default function Page() {
+  return (
+    <div>
+      <p>
+        <span className="text-2xl font-bold">Customers</span>
+        <span className="text-sm font-normal text-gray-500">
+          {" "}
+          - Manage your customers
+        </span>
+      </p>
+    </div>
+  );
+}
---
+++ finsync-dashboard/app/dashboard/invoices/page.tsx
@@ -0,0 +1,13 @@
+export default function Page() {
+  return (
+    <div>
+      <p>
+        <span className="text-2xl font-bold">Invoices</span>
+        <span className="text-sm font-normal text-gray-500">
+          {" "}
+          - Manage your invoices
+        </span>
+      </p>
+    </div>
+  );
+}
